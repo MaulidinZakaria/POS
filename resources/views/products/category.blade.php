d71c316bea702ebd55637df53fd3220d44f90e2e
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2024-02-29 21:17:28 +0700

    Pengaturan database dan file env

--- resources/views/products/category.blade.php
+++ resources/views/products/category.blade.php
@@ -3,7 +3,8 @@
 <body>
     <h1>Halaman Category {{ $nameProduct }}</h1>
     <br><br>
-    <h3><a href="{{url('/category')}}">Back</a></h3>
+    <h3><a href="{{url('/category')}}">Back
+</a></h3>
     @include('templates.footer')
 </body>
 
