10ac834122e09f9664658441438e0b23a5467bb6
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2024-03-01 23:30:08 +0700

    adding tailwind css

--- resources/views/level.blade.php
+++ resources/views/level.blade.php
@@ -4,20 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <h1>Data Level Pengguna</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
+    <table class=" border border-black border-spacing-2">
         <tr>
-            <th>ID</th>
-            <th>Kode Level</th>
-            <th>Nama Level</th>
+            <th class="border border-black p-2">ID</th>
+            <th class="border border-black p-2">Kode Level</th>
+            <th class="border border-black p-2">Nama Level</th>
         </tr>
         @foreach ($data as $d)
         <tr>
-            <td>{{ $d->level_id }}</td>
-            <td>{{ $d->level_kode }}</td>
-            <td>{{ $d->level_nama }}</td>
+            <td class="border border-black p-2">{{ $d->level_id }}</td>
+            <td class="border border-black p-2">{{ $d->level_kode }}</td>
+            <td class="border border-black p-2">{{ $d->level_nama }}</td>
         </tr>
         @endforeach
     </table>
