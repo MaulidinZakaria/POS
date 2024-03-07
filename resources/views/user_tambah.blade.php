e6ac2784fe45fd1e68d89ac3cd79f96af3fd8756
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2024-03-07 22:31:31 +0700

    Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

---
+++ resources/views/user_tambah.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data</title>
+</head>
+
+<body>
+    <h1>Form Tambah Data User</h1>
+    <a href="{{ route('/user') }}">Kembali</a>
+    <form action="{{ route('/user/tambah_simpan') }}" method="post">
+        {{ csrf_field() }}
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Masukkan Username">
+        <br>
+        <label>Nama</label>
+        <input type="text" name="nama" placeholder="Masukkan Nama">
+        <br>
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Masukkan Password">
+        <br>
+        <label>Level ID</label>
+        <input type="number" name="level_id">
+        <br>
+        <input type="submit" value="Simpan" name="btn btn-success">
+    </form>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,13 @@ use Monolog\Level;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::prefix('category')->group(function () {
-    Route::get('/', [ProductsController::class, 'index']);
-    Route::get('/food-beverage', [ProductsController::class, 'category1']);
-    Route::get('/beauty-health', [ProductsController::class, 'category2']);
-    Route::get('/home-care', [ProductsController::class, 'category3']);
-    Route::get('/baby-kid', [ProductsController::class, 'category4']);
-});
+// Route::prefix('category')->group(function () {
+//     Route::get('/', [ProductsController::class, 'index']);
+//     Route::get('/food-beverage', [ProductsController::class, 'category1']);
+//     Route::get('/beauty-health', [ProductsController::class, 'category2']);
+//     Route::get('/home-care', [ProductsController::class, 'category3']);
+//     Route::get('/baby-kid', [ProductsController::class, 'category4']);
+// });
 
 // Route::get('/user/{id}/name/{name}', [UserController::class, 'index']);
 
@@ -36,4 +36,12 @@ Route::get('/selling', [SellingController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-ROute::get('/user', [UserController::class, 'index']);
+Route::get('/user', [UserController::class, 'index'])->name('/user');
+
+
+Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('/user/ubah');
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('/user/hapus');
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
+
