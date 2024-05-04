de33b825be489c4fa4c814a70124f51b83f3b6ae
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2024-05-04 23:06:42 +0700

    Praktikum 11 API-2

--- app/Http/Controllers/Api/BarangController.php
+++ app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -15,8 +16,38 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+        $validator = Validator::make($request->all(), [
+            'barang_kode' => 'required|string|min:3|unique:m_barang,barang_kode',
+            'barang_nama' => 'required|string|max:100',
+            'harga_beli' => 'required|integer',
+            'harga_jual' => 'required|integer',
+            'kategori_id' => 'required|integer',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $barang = BarangModel::create([
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'kategori_id' => $request->kategori_id,
+            'image' => $request->image->hashName(),
+        ]);
+
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function show(BarangModel $barang)
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -27,7 +28,7 @@ class UserModel extends Authenticable implements JWTSubject
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     public $timestamps = false;
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
     public function level(): BelongsTo
     {
@@ -37,5 +38,11 @@ class UserModel extends Authenticable implements JWTSubject
     public function stok(): HasMany
     {
         return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('storage/posts' . $image),
+        );
     }
 }
